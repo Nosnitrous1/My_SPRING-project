676c1c2cb57fee07dc23bcbad15be5eb79da8d8d
Nosnitrous1 <nosnitrous@yandex.ru>
2021-09-02 12:08:58 +0300

    Added variables surname and age in Person class + getters-setters. Values were assigned to this vars through applicationContext.xml

--- src/main/java/com/example/demo/spring_introduction/Person.java
+++ src/main/java/com/example/demo/spring_introduction/Person.java
@@ -1,18 +1,39 @@
 package com.example.demo.spring_introduction;
 
 public class Person {
-  private  Pet pet;
+  private Pet pet;
+  private String surname;
+  private int age;
 
-//  public Person(Pet pet) {
+  //  public Person(Pet pet) {
 //    this.pet = pet;
 //    System.out.println("Bean Person is created");
   public Person() {
     System.out.println("Bean Person is created");
   }
-    public void setPet(Pet pet) {
-      System.out.println("Class Person: set pet");
-      this.pet = pet;
-    }
+
+  public void setPet(Pet pet) {
+    System.out.println("Class Person: set pet");
+    this.pet = pet;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public void setSurname(String surname) {
+    this.surname = surname;
+    System.out.println("Class Person: set surname "+ this.surname);
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+    System.out.println("Class Person: set age "+this.age);
+  }
 
   public void callYourPet() {
     System.out.println("Hello, my lovely Pet!");
